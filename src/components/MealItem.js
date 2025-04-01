141a52fcf44369edfbbebed8c775ab430c6899dd
Reinar Kajava <reinar.kajava@voco.ee>
2025-04-01 11:41:05 +0300

    Ylesanne 5 - Andmete valuuta kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,24 @@
+//pildid toidust liigutati public kausta, et veebileht saaks neid kuvada. Ilma selleta töötas leht küll aga pilte ette ei kuvatid...
+
+
+
 const MealItem = (props) => {
+    
+
+    const formattedPrice = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(Number(props.meal.price)); // Ensure it's a number
+
+
+
     return (
       <li className="meal-item">
         <article>
           <img src={props.meal.image} alt={props.meal.name} />
           <div className="meal-item-description">
             <h3>{props.meal.name}</h3>
-            <p className="meal-item-price">${props.meal.price}</p>
+            <p className="meal-item-price">${formattedPrice}</p>
             <p>{props.meal.description}</p>
           </div>
           <div className="meal-item-actions">
