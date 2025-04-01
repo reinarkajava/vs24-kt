1e1aad6df7b7e37ed5a8c6c73a1d07c39ba20556
Reinar Kajava <reinar.kajava@voco.ee>
2025-04-01 09:46:04 +0300

    Ylesanne 1 Header ja Meals komponendid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from './components/Header';
+import Meals from "./components/Meals";
+
 const App = () => {
  return (
     <>
-     <h1>Food Order App</h1>
+     <Header/>
+     <Meals/>
+     <p>Create a list of meals"</p>
     </>
   );
 }
